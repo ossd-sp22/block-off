191bde9eb308237a315a97671288e9d9ef90ded4
Amanda <amanda@Amandas-MBP-8.cable.rcn.com>
2022-02-11 21:13:28 -0500

    main js file

---
+++ blockOff.js
@@ -0,0 +1,9 @@
+if (document.URL.startsWith("https://www.facebook.com/")){
+    window.location = "https://google.com"
+}
+if (document.URL.startsWith("https://www.youtube.com/")){
+    window.location = "https://google.com"
+}
+if (document.URL.startsWith("https://www.netflix.com/")){
+    window.location = "https://google.com"
+}
